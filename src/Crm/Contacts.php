ac0614d71701212f08674f53bd623bf5278ab39e
Weslley Ceresa <webflix.tecnologia@gmail.com>
2025-04-11 21:11:28 -0300

    detalhes do contato CRM

--- src/Crm/Contacts.php
+++ src/Crm/Contacts.php
@@ -4,6 +4,39 @@ namespace RdStation\Crm;
 
 class Contacts extends \RdStation\Core\RdStationCrm{
     
+    public function details($contactId){
+        $query['token'] = $this->getToken();
+        
+        try{
+            $response = $this->http->get(sprintf('contacts/%s', $contactId), [
+                "query" => $query,
+            ]);
+
+            $body = (string)$response->getBody();
+                        
+            return json_decode($body);
+            
+        } catch (\GuzzleHttp\Exception\ServerException $ex) {
+            
+            throw \RdStation\Exceptions\RdStationException::fromGuzzleException($ex);
+                        
+        } catch (\GuzzleHttp\Exception\ClientException $ex) {
+            
+            throw \RdStation\Exceptions\RdStationException::fromGuzzleException($ex);
+            
+        } catch (\GuzzleHttp\Exception\BadResponseException $ex) {
+            
+            throw \RdStation\Exceptions\RdStationException::fromGuzzleException($ex);
+            
+        } catch (\GuzzleHttp\Exception\RequestException $ex) {
+            
+            throw \RdStation\Exceptions\RdStationException::fromGuzzleException($ex);
+            
+        } catch (\Exception $ex) {
+            throw new \RdStation\Exceptions\RdStationException($ex);
+        }
+    }
+    
     public function list(array $query = []){
         $query['token'] = $this->getToken();
         
